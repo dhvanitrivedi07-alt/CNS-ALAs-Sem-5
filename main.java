340a22a9d7363e6bef3c980fb632a63826e97d7c
dhvanitrivedi07-alt <dhvanitrivedi07@gmail.com>
2026-04-19 19:16:52 +0530

    Delete main.java

--- main.java
+++
@@ -1,6 +0,0 @@
-public class Main { 
-public static void main(String[] args) { 
-double celsius=37.0; 
-double fahrenheit = (celsius * 9/5) + 32;
- System.out.printf("Fahrenheit = %.2f%n", fahrenheit); 
-} }
